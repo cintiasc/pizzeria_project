ef4a4cf01ae6e49150d9286fd1e6469968722c0e
Cintia da Silva Costa <cintiadasilvacosta@outlook.com>
2022-09-03 22:29:10 -0300

    So far, the following points have been made: login screen, functionality to log out of the application, and the construction of the dashboard screen was started

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Dashboard" 
+                component={Dashboard} 
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
+}
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+
+import { View, ActivityIndicator } from 'react-native';
+
+import { AuthContext } from "../contexts/AuthContext";
+import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
+
+function Routes(){
+    const { isAuthenticated, loading } = useContext(AuthContext);
+
+    if(loading){
+        return (
+            <View 
+                style={{
+                    flex:1, 
+                    backgroundColor: '#1D1D2E', 
+                    justifyContent: 'center', 
+                    alignItems:'center'
+                }} 
+            >
+                <ActivityIndicator size={60} color="#FFF" />
+            </View>
+        )
+    }
+    return (
+        isAuthenticated ? <AppRoutes /> : <AuthRoutes />
+    );
+}
+
+export default Routes;
